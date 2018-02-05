55d79ae303525714b5e6344c146d9879bfe89114
vigneshavm <avmvignesh0207@gmail.com>
2018-02-05 07:47:18 +0000

    mail sent

---
+++ webapps/js/register.js
@@ -0,0 +1,39 @@
+/**
+ * Created by vignesh on 2/5/18.
+ */
+
+
+function submitRequest(){
+
+    var mobileNo = $("#mobileNo").val();
+    var name = $("#name").val();
+    var Prefer = $("#Prefer").val();
+    var sel1 = $("#sel1").val();
+
+
+
+
+    var request ={
+        mobileNo : mobileNo,
+        name :name,
+        prefer : Prefer,
+        product :sel1
+
+    }
+
+
+    console.log("request",request)
+
+    $.ajax({url: "/submit",
+        data: request,
+
+
+        success: function(result){
+
+
+          /*  $("#mobileNo").val("");
+            $("#name").val("");
+            $("#Prefer").val("New");
+            $("#sel1").val("Washing machine");*/
+    }});
+}
